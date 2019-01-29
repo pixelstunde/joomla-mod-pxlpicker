25302a5afb3b0ff9f72832ffc743e840ca21fc02
pixelstunde <c.friedemann@pixelstun.de>
2019-01-29 15:33:53 +0100

    Added 2 layouts for header files

---
+++ tmpl/header_pagebuilder.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_pxlpicker
+ *
+ * @copyright   Christian Friedemann = https://pixelstun.de
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+
+defined( '_JEXEC' ) or die;
+?>
+<div class="header">
+    <?php if ( ! empty ( $url ) ): ?>
+        <a href="<?php echo $url ?>">
+    <?php endif; ?>
+
+    <img src="<?php echo $path ?>" alt="<?php echo $alt ?>" <?php if ($id): ?> id="<?php echo $id ?>" <?php endif; ?> class="<?php echo $class ?>"/>
+
+    <?php if ( ! empty ( $url ) ): ?>
+        </a>
+    <?php endif; ?>
+    <div class="sppb-container">
+        <div class="sppb-row">
+            <div class="sppb-col-sm-12">
+                <h2 class="title"> <?php echo $title ?></h2>
+            </div>
+        </div>
+    </div>
+</div>
