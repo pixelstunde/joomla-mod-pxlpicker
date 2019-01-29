b6d900dc311ef7b6a96e2589888cdb1e5b2a68e3
pixelstunde <c.friedemann@pixelstun.de>
2019-01-29 15:36:05 +0100

    Added background layout

---
+++ tmpl/background.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_pxlpicker
+ *
+ * @copyright   Christian Friedemann = https://pixelstun.de
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+
+defined('_JEXEC') or die;
+?>
+<div class="pxlpicker-bg" style="background: url('<?php echo $path ?>');width:100%;height:100%;">
+</div>
